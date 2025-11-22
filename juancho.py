a49471611bd1b887d1f199411ef97232e43311f0
juandavidpuellozambrano-prog <juandavidpuellozambrano@gmail.com>
2025-11-21 19:29:39 -0500

    Create juancho.py

---
+++ juancho.py
@@ -0,0 +1,9 @@
+"090  = (
+
+              "XD"  : "risa"
+              "idk" : "yo nose" )
+vord = input(" escribe una palabra que no entiendes (¡CON MAYUSCULAS!) ")
+ if world in none.keys():
+
+ else:
+     print("otra porfa")
